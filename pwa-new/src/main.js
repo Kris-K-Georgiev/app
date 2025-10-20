5d7e411869b74e4169711b05c6724b929394b069
Kristian <126570041+Kris-K-Georgiev@users.noreply.github.com>
2025-10-20 14:11:57 +0300

    ,

--- pwa-new/src/main.js
+++ pwa-new/src/main.js
@@ -1,17 +1,10 @@
 import { h, render } from 'htm/preact';
-import './styles.css'
 import { Router } from './router.js';
-
-
-render(h(Router), document.getElementById('app'));
-
-import { registerSW } from './sw-register.js'
+import 'flowbite-react';
+import 'idb-keyval';
 
 const App = () => h('div', {class: 'min-h-screen flex flex-col'}, [
   h(Router)
-])
+]);
 
-render(h(App), document.getElementById('app'))
-
-// register service worker
-registerSW()
+render(h(App), document.getElementById('app'));
