52ee3b08b61f3c28430a3cb5ac490258b8204e06
Kristian <126570041+Kris-K-Georgiev@users.noreply.github.com>
2025-10-20 14:00:24 +0300

    .

---
+++ pwa-new/src/main.js
@@ -0,0 +1,17 @@
+import { h, render } from 'htm/preact';import './styles.css'
+
+import { Router } from './router.js';import { h, render } from 'htm/preact'
+
+import { Router } from './router.js'
+
+render(h(Router), document.getElementById('app'));import { registerSW } from './sw-register.js'
+
+
+const App = () => h('div', {class: 'min-h-screen flex flex-col'}, [
+  h(Router)
+])
+
+render(h(App), document.getElementById('app'))
+
+// register service worker
+registerSW()
