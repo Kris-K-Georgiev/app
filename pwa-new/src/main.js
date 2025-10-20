e42971827ce44cc8b4be3149533bcb4206c84683
Kristian <126570041+Kris-K-Georgiev@users.noreply.github.com>
2025-10-20 14:05:58 +0300

    .

--- pwa-new/src/main.js
+++ pwa-new/src/main.js
@@ -1,15 +1,17 @@
 import { h, render } from 'htm/preact';
-import './styles.css';
+import './styles.css'
 import { Router } from './router.js';
-import { registerSW } from './sw-register.js';
 
-// Главният компонент
-const App = () => h('div', { class: 'min-h-screen flex flex-col' }, [
+
+render(h(Router), document.getElementById('app'));
+
+import { registerSW } from './sw-register.js'
+
+const App = () => h('div', {class: 'min-h-screen flex flex-col'}, [
   h(Router)
-]);
+])
 
-// Рендериране в #app
-render(h(App), document.getElementById('app'));
+render(h(App), document.getElementById('app'))
 
-// Регистриране на Service Worker
-registerSW();
+// register service worker
+registerSW()
